3ca5277f2d7b9c095b12753a2157494c5a8d02d8
Daniel <daratcliffe1@outlook.com>
2018-07-15 23:27:50 +0100

    Incomplete - added start of "Media" category

---
+++ SharpCast TV/Media/Live/CreateLiveMedia.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+// Not sure if this will work - if anyone who sees the code can let me know, that'd be great!
+
+namespace QuickType
+{
+    // Request to Smashcast
+    public class CreateLiveMediaRequest
+    {
+        [JsonProperty("user_name")]
+        public string UserName { get; set; }
+
+        [JsonProperty("authToken")]
+        public string AuthToken { get; set; }
+
+        [JsonProperty("media_type")]
+        public string MediaType { get; set; }
+
+        [JsonProperty("media_description")]
+        public string MediaDescription { get; set; }
+
+        [JsonProperty("media_status")]
+        public string MediaStatus { get; set; }
+
+        [JsonProperty("media_name")]
+        public string MediaName { get; set; }
+
+        [JsonProperty("media_category_id")]
+        public string MediaCategoryId { get; set; }
+    }
+
+    // Response from Smashcast
+    public class CreateLiveMediaResponse
+    {
+        [JsonProperty("media_id")]
+        public int MediaId { get; set; }
+    }
+}
---
+++ SharpCast TV/Media/Live/GetLiveMedia.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace SharpCast_TV.Media.Live
+{
+    class GetLiveMedia
+    {
+        [JsonProperty("request")]
+        public Request Request { get; set; }
+
+        [JsonProperty("media_type")]
+        public string MediaType { get; set; }
+
+        [JsonProperty("livestream")]
+        public IList<Livestream> Livestream { get; set; }
+
+        [JsonProperty("media_hosted_media")]
+        public IList<Livestream> Hosted_media { get; set; }
+    }
+
+    public class Request
+    {
+        [JsonProperty("this")]
+        public string This { get; set; }
+    }
+
+    public class Channel
+    {
+        [JsonProperty("followers")]
+        public string Followers { get; set; }
+
+        [JsonProperty("videos")]
+        public string Videos { get; set; }
+
+        [JsonProperty("recordings")]
+        public string Recordings { get; set; }
+
+        [JsonProperty("teams")]
+        public string Teams { get; set; }
+
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+
+        [JsonProperty("user_name")]
+        public string UserName { get; set; }
+
+        [JsonProperty("user_status")]
+        public string UserStatus { get; set; }
+
+        [JsonProperty("user_logo")]
+        public string UserLogo { get; set; }
+
+        [JsonProperty("user_cover")]
+        public string UserCover { get; set; }
+
+        [JsonProperty("user_logo_small")]
+        public string UserLogoSmall { get; set; }
+
+        [JsonProperty("user_partner")]
+        public string UserPartner { get; set; }
+
+        [JsonProperty("partner_type")]
+        public string PartnerType { get; set; }
+
+        [JsonProperty("user_beta_profile")]
+        public string UserBetaProfile { get; set; }
+
+        [JsonProperty("media_is_live")]
+        public string MediaIsLive { get; set; }
+
+        [JsonProperty("media_live_since")]
+        public string MediaLiveSince { get; set; }
+
+        [JsonProperty("user_media_id")]
+        public string UserMediaId { get; set; }
+
+        [JsonProperty("twitter_account")]
+        public string TwitterAccount { get; set; }
+
+        [JsonProperty("twitter_enabled")]
+        public string TwitterEnabled { get; set; }
+
+        [JsonProperty("livestream_count")]
+        public string LivestreamCount { get; set; }
+
+        [JsonProperty("channel_link")]
+        public string ChannelLink { get; set; }
+    }
+
+    public class Livestream
+    {
+        [JsonProperty("media_user_name")]
+        public string MediaUserName { get; set; }
+
+        [JsonProperty("media_id")]
+        public string MediaId { get; set; }
+
+        [JsonProperty("media_file")]
+        public string MediaFile { get; set; }
+
+        [JsonProperty("media_user_id")]
+        public string MediaUserId { get; set; }
+
+        [JsonProperty("media_profiles")]
+        public string MediaProfiles { get; set; }
+
+        [JsonProperty("media_type_id")]
+        public string MediaTypeId { get; set; }
+
+        [JsonProperty("media_is_live")]
+        public string MediaIsLive { get; set; }
+
+        [JsonProperty("media_live_delay")]
+        public string MediaLiveDelay { get; set; }
+
+        [JsonProperty("media_date_added")]
+        public string MediaDateAdded { get; set; }
+
+        [JsonProperty("media_live_since")]
+        public string MediaLiveSince { get; set; }
+
+        [JsonProperty("media_transcoding")]
+        public string MediaTranscoding { get; set; }
+
+        [JsonProperty("media_chat_enabled")]
+        public string MediaChatEnabled { get; set; }
+
+        [JsonProperty("media_countries")]
+        public IList<string> MediaCountries { get; set; }
+
+        [JsonProperty("media_offline_id")]
+        public object MediaOfflineId { get; set; }
+
+        [JsonProperty("media_hosted_id")]
+        public object MediaHostedId { get; set; }
+
+        [JsonProperty("media_mature")]
+        public object MediaMature { get; set; }
+
+        [JsonProperty("media_hidden")]
+        public string MediaHidden { get; set; }
+
+        [JsonProperty("user_banned")]
+        public object UserBanned { get; set; }
+
+        [JsonProperty("media_name")]
+        public string MediaName { get; set; }
+
+        [JsonProperty("media_display_name")]
+        public string MediaDisplayName { get; set; }
+
+        [JsonProperty("media_status")]
+        public string MediaStatus { get; set; }
+
+        [JsonProperty("media_title")]
+        public object MediaTitle { get; set; }
+
+        [JsonProperty("media_description")]
+        public string MediaDescription { get; set; }
+
+        [JsonProperty("media_description_md")]
+        public string MediaDescriptionMd { get; set; }
+
+        [JsonProperty("media_tags")]
+        public string MediaTags { get; set; }
+
+        [JsonProperty("media_duration")]
+        public object MediaDuration { get; set; }
+
+        [JsonProperty("media_bg_image")]
+        public string MediaBgImage { get; set; }
+
+        [JsonProperty("media_views")]
+        public string MediaViews { get; set; }
+
+        [JsonProperty("media_views_daily")]
+        public string MediaViewsDaily { get; set; }
+
+        [JsonProperty("media_views_weekly")]
+        public string MediaViewsWeekly { get; set; }
+
+        [JsonProperty("media_views_monthly")]
+        public string MediaViewsMonthly { get; set; }
+
+        [JsonProperty("category_id")]
+        public string CategoryId { get; set; }
+
+        [JsonProperty("category_name")]
+        public string CategoryName { get; set; }
+
+        [JsonProperty("category_name_short")]
+        public object CategoryNameShort { get; set; }
+
+        [JsonProperty("category_seo_key")]
+        public string CategorySeoKey { get; set; }
+
+        [JsonProperty("category_viewers")]
+        public string CategoryViewers { get; set; }
+
+        [JsonProperty("category_media_count")]
+        public string CategoryMediaCount { get; set; }
+
+        [JsonProperty("category_channels")]
+        public object CategoryChannels { get; set; }
+
+        [JsonProperty("category_logo_small")]
+        public object CategoryLogoSmall { get; set; }
+
+        [JsonProperty("category_logo_large")]
+        public string CategoryLogoLarge { get; set; }
+
+        [JsonProperty("category_updated")]
+        public string CategoryUpdated { get; set; }
+
+        [JsonProperty("team_name")]
+        public object TeamName { get; set; }
+
+        [JsonProperty("media_start_in_sec")]
+        public string MediaStartInSec { get; set; }
+
+        [JsonProperty("media_thumbnail")]
+        public string MediaThumbnail { get; set; }
+
+        [JsonProperty("media_thumbnail_large")]
+        public string MediaThumbnailLarge { get; set; }
+
+        [JsonProperty("channel")]
+        public Channel Channel { get; set; }
+    }
+}
